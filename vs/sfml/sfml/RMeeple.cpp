e3934b23233e4975f80b11635eca08060cbdb753
simon dimitriadis <pumba6@gmx.at>
2014-11-15 05:36:23 +0100

    meeples are almost in the game .. they glow, can be dragged and init on the right position

--- vs/sfml/sfml/RMeeple.cpp
+++ vs/sfml/sfml/RMeeple.cpp
@@ -1,10 +1,98 @@
 #include "RMeeple.h"
 
-#pragma warning( disable: 4100 )
+const float MEEPLE_HIGHT = 170;
+const float MEEPLE_WIDTH = 80;
 
-RMeeple::RMeeple(const Meeple& logicalMeeple, sf::RectangleShape* graficRepresentation){
+//logicalMeeple has to be deleted by the bag
+RMeeple::RMeeple(const Meeple& logicalMeeple, sf::Texture& shapeTex, sf::IntRect& shapeTexRect, sf::Texture& glowShapeTex, sf::IntRect& glowShapeTexRect, sf::Vector2f& initPos)
+	: logicalMeeple(&logicalMeeple), isDragged(false), glow(false)
+{
+	sf::Vector2f size(MEEPLE_WIDTH, MEEPLE_HIGHT);
+
+	shape.setSize(size);
+	shape.setTexture(&shapeTex);
+	shape.setTextureRect(shapeTexRect);
+
+	glowShape.setSize(size);
+	glowShape.setTexture(&glowShapeTex);
+	glowShape.setTextureRect(glowShapeTexRect);
+
+	updatePosition(initPos);
+}
+
+RMeeple::~RMeeple(){ }
+
+
+void RMeeple::updatePosition(const sf::Vector2f& newPosition){
+	shape.setPosition(newPosition.x,newPosition.y);
+	glowShape.setPosition(newPosition.x, newPosition.y);
 }
 
 
-RMeeple::~RMeeple(){
+void RMeeple::draw(sf::RenderWindow& window){
+	window.draw(shape);
+	if (glow){
+		window.draw(glowShape);
+	}
+	
 }
+
+void RMeeple::shouldGlow(sf::Vector2f& converted){
+	if (shape.getGlobalBounds().contains(converted)){
+		glow = true;
+	}
+	else{
+		glow = false;
+	}
+}
+
+bool RMeeple::containsMousePos(sf::Vector2f& converted){
+	return shape.getGlobalBounds().contains(converted);
+
+		 //true;
+		//isDragged = true;
+		////return 
+		//sf::Vector2f vect;
+		//vect.x = converted.x - shape.getGlobalBounds().left;
+		//vect.y = converted.y - shape.getGlobalBounds().top;
+		//return vect;
+	//}
+	//else{
+		//isDragged = false;
+	//}
+}
+
+sf::FloatRect RMeeple::getGlobalBounds(){
+	return shape.getGlobalBounds();
+}
+
+bool RMeeple::getIsDragged(){
+	return this->isDragged;
+}
+
+void RMeeple::setIsDragged(bool isDragged){
+	this->isDragged = isDragged;
+}
+
+bool RMeeple::getGlow(){
+	return this->glow;
+}
+
+void RMeeple::setGlow(bool glow){
+	this->glow = glow;
+}
+
+sf::Vector2f* RMeeple::getMoveToPosition(){
+	return &this->moveToPosition;
+}
+
+void RMeeple::setMoveToPosition(sf::Vector2f& moveToPosition){
+	this->moveToPosition.x = moveToPosition.x;
+	this->moveToPosition.y = moveToPosition.y;
+}
+
+
+
+
+
+
--- vs/sfml/sfml/main.cpp
+++ vs/sfml/sfml/main.cpp
@@ -15,29 +15,16 @@
 
 #include "Menu.h"
 
-//using namespace std;
+const std::string GAME_TITLE = "4 Wins by ...";
 
-
-#define GAME_TITLE "4 Wins"
-
-
-
-
-//+++++++++++++++++++++
-#define MEEPLE_HIGHT 90
-#define MEEPLE_WIDTH 45
-
-#define MEEPLE_SNAP_X 22
-#define MEEPLE_SNAP_Y 65
-
-#define MEEPLE_SNAP_OFFSET_X 12
-#define MEEPLE_SNAP_OFFSET_Y 30
-//membervars!!!!
-//+++++++++++++++++++++
-
-
-sf::RenderWindow* setupWindow();
-
+sf::RenderWindow* setupWindow(){
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH_TO_CALCULATE, WINDOW_HEIGHT_TO_CALCULATE), GAME_TITLE);
+	window->setPosition(sf::Vector2i(0, 0));
+	window->setVerticalSyncEnabled(true); //entweder das oder set frameratelimit
+	//window->setFramerateLimit(30);
+	window->setSize(sf::Vector2u(WINDOW_WIDTH, WINDOW_HEIGHT));// 16:9
+	return window;
+}
 
 void AI_testFunction(){     
     I_Player* p1 = new StupidAI();// (true, true); //ThinkingAI(true, true);
@@ -55,27 +42,24 @@ void AI_testFunction(){
     exit(0);
 }
 
-
-
-
-
 int main(){
-
-	AI_testFunction();
+	//AI_testFunction();
 	
 	sf::RenderWindow* window = setupWindow();
-	
 	Menu* menu = new Menu(window);
-	
 	while (window->isOpen()){
 		
-		GameSettings gamesettings = menu->loop();
+		//GameSettings gamesettings = menu->loop();
 		//system("pause");
 		
-		Game* game = new Game(*window, *gamesettings.playerOne,*gamesettings.playerTwo);
+		I_Player* test =	new ThinkingAI(true, true);
+		I_Player* test2 = new ThinkingAI(true, true);
+
+		Game* game = new Game(*window, *test,*test2);
 		game->runGame();
 
 		delete game;
+		break; // or start new game .. 
 
 	}
 
@@ -85,24 +69,6 @@ int main(){
 	return 0;
 }
 
-sf::RenderWindow* setupWindow(){
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML Application");
-	window->setPosition(sf::Vector2i(0, 0));
-	window->setVerticalSyncEnabled(true); //entweder das oder set frameratelimit
-
-	return window;
-}
-
-//srand(static_cast<unsigned int>(time(NULL)));
-
-//AI_testFunction();
-
-//--------------------
-//membervars
-/*	bool dragme = false;
-sf::Vector2f dragmeVect(0, 0);
-sf::Vector2f oldPos(30, 30);
-*///--------------------
 
 
 
@@ -118,110 +84,21 @@ cin.ignore();   //wait for keypress
 music.stop();
 return 0;
 */
-//bool keypressedK = false;
-//bool glow = false;
-
-//	//window.setFramerateLimit(30);
-//sf::RectangleShape leftPanel(sf::Vector2f(200.f,690.f));
-//leftPanel.setPosition(sf::Vector2f(0.f, 0.f));
-//leftPanel.setFillColor(sf::Color::Green);
-
-//sf::RectangleShape rightPanel(sf::Vector2f(200.f,690.f));
-//rightPanel.setPosition(sf::Vector2f(1150.f, 0.f));
-//rightPanel.setFillColor(sf::Color::Magenta);
 
 
-//sf::RectangleShape boardPanel(sf::Vector2f(450.f, 450.f));
-//boardPanel.setFillColor(sf::Color::White);
-//boardPanel.setPosition(sf::Vector2f(BOARD_X_OFFSET, BOARD_Y_OFFSET- 95.f));
-//boardPanel.setRotation(45.f);
-//boardPanel.setRotation(45.f);
-
-
-//sf::CircleShape* squares = new sf::CircleShape[16]();
-
-//
-//int counter = 0;
-//for (int y= 0; y < 7; ++y){
-
-//	//cout << "counter in loop: " << counter << endl;
-//	
-
-//	int times = (y + 1) > 3 ? 7 - y  : y + 1;
-//	cout << "times: "  << times << endl;
-//	
-//	float calcX = BOARD_X_OFFSET - ((times -1)/2.f *BOARD_X_SPACEING) ;
-
-//	for (int x = 0; x < times; ++x){	
-//		squares[counter].setOrigin(sf::Vector2f(40.f, 40.f));
-//		squares[counter].setFillColor(sf::Color::Yellow);
-//		squares[counter].setRadius(40);
-//		squares[counter].setPointCount(6);
-//		squares[counter].setPosition(sf::Vector2f(calcX, y * BOARD_Y_SPACEING + BOARD_Y_OFFSET));
-//		calcX += BOARD_X_SPACEING;
-//		++counter;
-//	}
-//}
-//
-//
-//sf::RectangleShape meeple(sf::Vector2f(MEEPLE_WIDTH,MEEPLE_HIGHT));
-//
-//sf::Texture texture;
-//// load a 32x32 rectangle that starts at (10, 10)
-//if (!texture.loadFromFile(WORKING_DIR+"field.jpg"))
-//{
-//	//error
-//
-//}
-//texture.setRepeated(true);
-////textur
-//cout << "load tex..." << endl;
-//meeple.setTexture(&texture);
-//meeple.setTextureRect(sf::IntRect(10, 10, 32, 32));
-
-////meeple.setTexture(&texture);
-
-////meeple.setFillColor(sf::Color::Blue);
-//meeple.setPosition(30,30);
-
-//while (window->isOpen()){
-//
-//	sf::Event event;
-//	while (window->pollEvent(event)){
-
-//		if (event.type == sf::Event::Closed){
-//			window->close();
-//		}
-
-//		if (event.type == sf::Event::KeyPressed)
-//		{
-//			if (event.key.code == sf::Keyboard::K)
-//			{
-//				cout << "key k pressed " << endl;
-//				keypressedK = true;
-//			}
-//		}
-//		if (event.type == sf::Event::KeyReleased)
-//		{
-//			if (event.key.code == sf::Keyboard::K)
-//			{
-//				cout << "key k released " << endl;
-//				keypressedK = false;
-//			}
-//		}
-//		if (event.type == sf::Event::MouseWheelMoved)
-//		{
-//			//std::cout << "wheel movement: " << event.mouseWheel.delta << std::endl;
-//			//std::cout << "mouse x: " << event.mouseWheel.x << std::endl;
-//			//std::cout << "mouse y: " << event.mouseWheel.y << std::endl;
-//		}
+//--------------------
+//membervars
+/*	bool dragme = false;
+sf::Vector2f dragmeVect(0, 0);
+sf::Vector2f oldPos(30, 30);
+*///--------------------
 
 //		if (event.type == sf::Event::MouseButtonReleased){
 //			if (event.mouseButton.button == sf::Mouse::Left){
 //				dragme = false;
 //				sf::Vector2f meeplePos(meeple.getGlobalBounds().left + MEEPLE_SNAP_X, meeple.getGlobalBounds().top +MEEPLE_SNAP_Y);
 //				for (int i = 0; i < 16; ++i){
-
+//
 //					if (squares[i].getGlobalBounds().contains(meeplePos)){
 //						cout << "snap to: " << i << endl;
 //						
@@ -231,8 +108,6 @@ return 0;
 //							//oldPos = sf::Vector2f(squares[i].getGlobalBounds().left+ MEEPLE_SNAP_OFFSET_X, squares[i].getGlobalBounds().top- MEEPLE_SNAP_OFFSET_Y);
 //					
 //					}
-//					
-
 //				}
 //				//if ( meeple.getGlobalBounds())
 //				meeple.setPosition(oldPos);
@@ -264,14 +139,6 @@ return 0;
 //				//std::cout << "mouse x: " << event.mouseButton.x << std::endl;
 //				//std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 //			}
-//			if (event.mouseButton.button == sf::Mouse::Right)
-//			{
-//				
-//				//std::cout << "the right button was pressed" << std::endl;
-//				//std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-//				//std::cout << "mouse y: " << event.mouseButton.y << std::endl ;
-//			}
-//		}
 //		if (event.type == sf::Event::MouseMoved)
 //		{
 //			sf::Vector2i mousepos = sf::Mouse::getPosition(*window);
@@ -305,25 +172,3 @@ return 0;
 //			//std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
 //			//std::cout << "new mouse y: " << event.mouseMove.y << std::endl << endl;
 
-//		}
-//	}
-
-//window->clear(sf::Color::Black);
-//window->draw(boardPanel);
-
-
-//for (int i = 0; i < 16; ++i){
-//	window->draw(squares[i]);
-//	//squares[i].setPointCount(5);
-//}
-
-//window->draw(rightPanel);
-//window->draw(leftPanel);
-//window->draw(meeple);
-//window->display();
-
-
-//delete window;
-//return 0;
-
-//}
